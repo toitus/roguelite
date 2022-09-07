3747912b60d9f203ddbb5ae227556c2690915c36
Curtis Beasley <cjbeasley10@gmail.com>
2022-09-06 17:49:12 -0700

    initial player class

---
+++ player.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Player {
+
+    public:
+
+        void events(sf::Event* event);
+        void update();
+        void draw(sf::RenderWindow* window);
+
+    private:
+
+        int row;
+        int column;
+        int size = 16;
+
+};
